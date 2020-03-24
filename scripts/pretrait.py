4b21b3cca07e88a08859226989657e3805220236
lpschaub <lp.schaub@gmail.com>
2020-03-24 22:04:17 +0100

    ajout pretrait predict

--- scripts/pretrait.py
+++ scripts/pretrait.py
@@ -28,39 +28,34 @@ from sklearn.metrics import accuracy_score, classification_report
 def pipeline(file) : 
 
     Corpus = pd.read_csv(file, sep='\t')
+    corpus = Corpus.reindex(np.random.permutation(Corpus.index))
+    print(corpus.head())
     # Step - a : Remove blank rows if any.
-    
+    print(corpus['text'])
+    print(corpus['label'])
 
     Corpus['text'].dropna(inplace=True)# Step - b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
     
+    print(type(Corpus['text']))
+    for elem in Corpus['text'] : 
+        print(elem)
+        print(type(elem))
+        break
 
     Corpus['text'] = [entry.lower() for entry in Corpus['text']]# Step - c : Tokenization : In this each entry in the corpus will be broken into set of words
-    # Corpus['text']= [str(word_tokenize(entry)) for entry in Corpus['text']]# Step - d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
-    # tag_map = defaultdict(lambda : wn.NOUN)
-    # tag_map['J'] = wn.ADJ
-    # tag_map['V'] = wn.VERB
-    # tag_map['R'] = wn.ADV
-    # for index,entry in enumerate(Corpus['text']):
-    #     # Declaring Empty List to store the words that follow the rules for this step
-    #     Final_words = []
-    # # Initializing WordNetLemmatizer()
-    #     word_Lemmatized = WordNetLemmatizer()
-    #     # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
-    #     for word, tag in pos_tag(entry):
-    #         # Below condition is to check for Stop words and consider only alphabets
-    #         if word not in stopwords.words('english') and word.isalpha():
-    #             word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
-    #             Final_words.append(str(word_Final))
-    # # The final processed set of words for each iteration will be stored in 'text_final'
-    # Corpus.loc[index,'text_final'] = str(Final_words)
+    
 
     Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text'],Corpus['label'],test_size=0.2)
+    print(len(Train_X))
+    print(len(Train_Y))
+
     Encoder = LabelEncoder()
-    # print(Train_Y)
     Train_Y = Encoder.fit_transform(Train_Y)
-    # print(Train_Y)
+    print(Train_Y)
 
     Test_Y = Encoder.fit_transform(Test_Y)
+    print(Test_Y)
+    sys.exit()
 
 
     representation = CorpusCSV(Corpus['text'])
