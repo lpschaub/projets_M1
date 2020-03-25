32395f99333f2bd25955c8a0708d610297618b3d
Camille Rey <camillerey@MacBook-Air-4.local>
2020-03-25 10:55:10 +0100

    ajout devoir BOW csv Camille

--- scripts/exercice_BOW_Camille/pretrait2_camille.py
+++ scripts/exercice_BOW_Camille/pretrait2_camille.py
@@ -2,8 +2,9 @@
 
 from glob import glob
 import os
+import random
 # from scripts.Evaluation import Evaluation
-from PredictBOW_cam import *
+from PredictBOW_cam_V2 import *
 #from Evaluation import Evaluation
 import spacy
 predicted = []
@@ -28,6 +29,19 @@ from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score, classification_report
 
 
+def pipeline(fic):
+    corpus=pd.read_csv(fic,sep='\t')
+    corpus['text']=[elem.lower() for elem in corpus['text']]
+    return corpus
+    #print(corpus.head())
+    #Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(corpus['text'],corpus['label'],test_size=0.2)
+    #Encoder = LabelEncoder()
+    #Train_Y = Encoder.fit_transform(Train_Y)
+
+
+    #Test_Y = Encoder.fit_transform(Test_Y)
+    #print(Train_Y)
+    #print(Test_Y)
 
 def files2csv(text,out,label):
 
@@ -70,13 +84,22 @@ def prediction(file,expect) :
     expected.append(expect)
 
 
+
+
 if __name__ == '__main__':
 
-    with open("./csv_test.csv", 'w') as file:
-        file.write("text\tlabel\n")
-    for fic in glob('../../corpus/imdb/*/*.txt') :
-        text,label=getcontentlabel(fic)
-        files2csv(text,"./csv_test.csv",label)
+    #with open("./csv_test.csv", 'w') as file:
+        #file.write("text\tlabel\n")
 
+    #for fic in random.sample(glob('../../../imdb_test/*/*.txt'),len(glob('../../../imdb_test/*/*.txt'))) :
+        #text,label=getcontentlabel(fic)
+        #files2csv(text,"./csv_test.csv",label)
+
+    corpus=CorpusCSV(pipeline("./csv_test.csv"))
+    print(corpus.voc)
+    corpus.getBOW()
+    print(corpus.bow)
+    print(corpus.bigram)
+    print(corpus.trigram)
 
     print("ca marche ! bravo")
